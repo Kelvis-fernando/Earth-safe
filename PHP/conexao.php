a2ddb283477f2882516fb6c776776055207699bb
Kelvis Fernando <kelvisdelapena@gmail.com>
2021-04-29 14:40:13 -0300

    Configurado o e adicionado ao projeto o banco de dados mySQL no arquivo conexao

---
+++ PHP/conexao.php
@@ -0,0 +1,10 @@
+<?php
+
+define('HOST', '127.0.0.1');
+define('USUARIO', 'root');
+define('SENHA', '');
+define('DB', 'loginearthsafe');
+
+$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possivel conectar');
+
+?>
