b3c04c3de13d91bfe9979c44b9df4b41f98b51f2
Kelvis Fernando <kelvisdelapena@gmail.com>
2021-05-23 23:38:36 -0300

    Refatorado a sidebar da pagina, refeita inteira com bootstrap, e responsiva

--- views/plataforma.php
+++ views/plataforma.php
@@ -1,5 +1,5 @@
 <?php
-    include('../php/verifica_login.php');
+include('../php/verifica_login.php');
 ?>
 
 <html lang="en">
@@ -8,42 +8,73 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style-plataforma.css">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="../css/plataforma.css">
     <link href="https://fonts.googleapis.com/css2?family=Antonio:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
     <title>Plataforma - Home</title>
 </head>
 
 <body>
-    <header>
-        <aside id="side-bar">
-            <img src="../img/logo.svg" alt="Logo" width="200">
-            <ul>
-                <li>
-                    <a href="" class="link-side-bar">Home</a>
-                </li>
-                <li>
-                    <a href="../views/login.php" class="link-side-bar">Plataforma</a>
-                </li>
-                <li>
-                    <a href="" class="link-side-bar">Crm</a>
-                </li>
-                <li>
-                    <a href="" class="link-side-bar">Clientes</a>
-                </li>
-            </ul>
-        </aside>
 
-        <nav id="navigation-plat">
-            <a href="../php/logout.php">Logout</a>
-        </nav>
-    </header>
-    <div class="clear"></div>
-    <main id="content-home">
-        <h2>Seja bem vindo <?php print_r($_SESSION);?></h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto magni officia porro ab earum ipsam commodi repellendus laborum fuga, beatae eum exercitationem aliquid excepturi soluta ullam ipsa temporibus consequatur quis.</p>
-    </main>
+    <body id="body-pd">
+        <header class="header" id="header">
+            <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
+            <div class="header_img"> <img src="https://i.imgur.com/hczKIze.jpg" alt=""> </div>
+        </header>
+        <div class="l-navbar" id="nav-bar">
+            <nav class="nav">
+                <div>
+                    <a href="#" class="nav_logo">
+                        <i class='bx bx-layer nav_logo-icon'></i>
+                        <span class="nav_logo-name">Earth Safe</span>
+                    </a>
+                    <div class="nav_list">
+                        <a href="#" class="nav_link active">
+                            <i class='bx bx-grid-alt nav_icon'></i>
+                            <span class="nav_name">Dashboard</span>
+                        </a>
+                        <a href="#" class="nav_link">
+                            <i class='bx bx-user nav_icon'></i>
+                            <span class="nav_name">Users</span>
+                        </a>
+                        <a href="#" class="nav_link">
+                            <i class='bx bx-message-square-detail nav_icon'></i>
+                            <span class="nav_name">Messages</span>
+                        </a>
+                        <a href="#" class="nav_link">
+                            <i class='bx bx-bookmark nav_icon'></i>
+                            <span class="nav_name">Bookmark</span>
+                        </a>
+                        <a href="#" class="nav_link">
+                            <i class='bx bx-folder nav_icon'></i>
+                            <span class="nav_name">Files</span>
+                        </a>
+                        <a href="#" class="nav_link">
+                            <i class='bx bx-bar-chart-alt-2 nav_icon'></i>
+                            <span class="nav_name">Stats</span>
+                        </a>
+                    </div>
+                </div>
+                <a href="#" class="nav_link">
+                    <i class='bx bx-log-out nav_icon'></i>
+                    <span class="nav_name">SignOut</span>
+                </a>
+            </nav>
+        </div>
 
-</body>
+        <!--Container Main start-->
+        <div class="height-100 bg-light">
+            <h4>Main Components</h4>
+            <main id="content-home">
+            <h2>Seja bem vindo <?php print_r($_SESSION); ?></h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto magni officia porro ab earum ipsam commodi repellendus laborum fuga, beatae eum exercitationem aliquid excepturi soluta ullam ipsa temporibus consequatur quis.</p>
+        </main>
+        </div>
+        <!--Container Main end-->
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+        <script src="../js/app.js"></script>
+    </body>
 
 </html>
